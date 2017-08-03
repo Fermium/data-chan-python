84aab29152da40537e7b56abe955779eb990acd0
Davide Bortolami <d@fermiumlabs.com>
2017-08-03 17:01:36 +0200

    update metadata

--- setup.py
+++ setup.py
@@ -1,19 +1,41 @@
 from setuptools import setup
 
+
 def readme():
     with open('README.rst') as f:
         return f.read()
-        
-        
+
+
 setup(name='data_chan',
-      version='0.1.9',
+      version='0.1.10',
       description='Python bindings for Data-Chan',
       long_description=readme(),
       url='https://github.com/fermiumlabs/data-chan-python/',
       author='Fermium LABS',
       author_email='info@fermiumlabs.com',
-      license='MIT',
+      license='Apache 2',
       packages=['data_chan'],
       zip_safe=False,
       include_package_data=True,
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
+
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic: : Software Development :: Build Tools',
+
+          # Pick your license as you wish (should match "license" above)
+          'License :: OSI Approved :: Apache Software License',
+
+          'Programming Language :: Python :: 3',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: MacOS',
+          'Operating System :: POSIX :: Linux',
+          'Operating System :: Unix'
+      ]
       )
