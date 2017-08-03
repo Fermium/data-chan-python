5148adaac3b79ecece0593f8f37aac92357d6ec6
Davide Bortolami <d@fermiumlabs.com>
2017-08-03 15:38:01 +0200

    Created python package

--- test.py
+++
@@ -1,9 +0,0 @@
-import halltester as ht
-
-ht.init()
-scan = ht.acquire(0x16d0,0x0c9b)
-ht.enable(scan)
-ht.set_current_lockin(scan,0.5)
-ht.set_heater_state(scan,200)
-ht.reset_device(scan)
-ht.pop_measure(scan)
