3d1185675f5a41c40f392ce5b221e4deb1f2154b
Simone Tosato <s@fermiumlabs.com>
2017-12-19 18:35:12 +0100

    bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='data_chan',
-      version='0.2.4',
+      version='0.2.5',
       description='Python bindings for Data-Chan',
       long_description=readme(),
       url='https://github.com/fermiumlabs/data-chan-python/',
