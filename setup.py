dab90139e15eae2251872eedde43e50c689e490f
Davide Bortolami <d@fermiumlabs.com>
2017-12-21 11:43:33 +0100

    BUUUUMP

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='data_chan',
-      version='0.2.8',
+      version='1.0.0',
       description='Python bindings for Data-Chan',
       long_description=readme(),
       url='https://github.com/fermiumlabs/data-chan-python/',
